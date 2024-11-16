b26b3a7a3ab0ae886714403cfd142b9180615b1d
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-11-16 13:34:36 -0300

    Implement reviewed repositories list with FlatList
    - Included static data for repositories.
    - Created RepositoryList and RepositoryItem components to display a list of repositories.
    - Used React Native's FlatList for efficient rendering of the repository list.
    - Rendered RepositoryList in the Main component.

---
+++ src/components/ItemSeparator.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet, View } from 'react-native'
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 8,
+  },
+})
+
+const ItemSeparator = () => <View style={styles.separator} />
+
+export default ItemSeparator
