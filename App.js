137b1310c8fff7ee56ce167a3b40e9b02063c72e
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-11-23 10:06:08 -0300

    Resolve React Router v7 splat path warning
    Added future flags (v7_startTransition, v7_relativeSplatPath) in NativeRouter.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar'
 import { NativeRouter } from 'react-router-native'
+import { StatusBar } from 'expo-status-bar'
 import { ApolloProvider } from '@apollo/client'
 import { PaperProvider } from 'react-native-paper'
 import createApolloClient from './src/utils/apolloClient'
@@ -13,7 +13,12 @@ const apolloClient = createApolloClient(authStorage)
 const App = () => {
   return (
     <>
-      <NativeRouter>
+      <NativeRouter
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}
+      >
         <ApolloProvider client={apolloClient}>
           <AuthStorageContext.Provider value={authStorage}>
             <PaperProvider>
